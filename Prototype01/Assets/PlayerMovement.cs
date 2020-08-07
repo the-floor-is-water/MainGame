745e5643ef177d179cf56855731cc3f2279ebd67
Alonsowski <santanasamuel29@gmail.com>
2020-08-07 16:32:30 -0500

    Saltar y correr
    Se agregaron las funcionalidades de correr (Sprint) y saltar (Jump) utilizando los parámetros de Unity de entrada (Input Manager)

--- Prototype01/Assets/PlayerMovement.cs
+++ Prototype01/Assets/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
@@ -6,14 +7,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float mSpeed = 4f;
+    public float walkSpeed = 4f;
     public float gravity = -9.81f;
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.5f;
     public LayerMask groundMask;
+    public float jumpHeight = 3f;
+    public float sprintSpeed = 8f;
 
     Vector3 velocity;
     bool isGrounded;
+    float movementSpeed = 0f;
 
     // Update is called once per frame
     void Update()
@@ -24,13 +28,27 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
+
         float X = Input.GetAxis("Horizontal");
         float Z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * X + transform.forward * Z;
-        controller.Move(move * mSpeed * Time.deltaTime);
+        if(Input.GetButton("Sprint"))
+        {
+            controller.Move(move * sprintSpeed * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(move * walkSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Convert.ToSingle(Math.Sqrt(jumpHeight * -2f * gravity));
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
 
     }
 }
