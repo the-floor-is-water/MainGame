6264181b69fb5039fb4d222bd43a06946379fe22
Alonsowski <santanasamuel29@gmail.com>
2020-08-24 21:31:33 -0500

    Trabajo en inventario
    Se hicieron un par de ajustes generales, se agregaron las imágenes para el inventario de los tres objetos y se desarrolló scripting independiente para manejar el inventario (en proceso)

---
+++ Prototype01/Assets/Scripts/TFIWItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface TFIWItem
+{
+    String Name { get; }
+    Sprite Image { get; }
+    int Category { get; }
+
+    void OnPickUp();
+    //void OnDrop();
+}
+
+public class TFIWItemEventArgs : EventArgs
+{
+    public TFIWItem Item;
+
+    public TFIWItemEventArgs(TFIWItem item)
+    {
+        Item = item;
+    }
+
+}
---
+++ Prototype01/Assets/Scripts/inventory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventory : MonoBehaviour
+{
+    public TFIWItem firstItem;
+    public TFIWItem secondItem;
+    public TFIWItem extraItem;
+
+    public event EventHandler<TFIWItemEventArgs> ItemAdded;
+
+    public void AddItem(TFIWItem item)
+    {
+        Collider collider = (item as MonoBehaviour).GetComponent<Collider>();
+        if(collider.enabled)
+        {
+            collider.enabled = false;
+            if(item.Category == 1)
+            {
+                if (secondItem == null)
+                    secondItem = item;
+                else
+                {
+                    if(firstItem != null)
+                    {
+                        //TODO: firstItem.OnDrop(); - Item exchange funtionality
+                    }
+                    firstItem = item;
+                }
+                item.OnPickUp();
+            }
+            else if(item.Category == 2)
+            {
+                if (extraItem != null)
+                {
+                    //TODO: firstItem.OnDrop(); - Item exchange funtionality
+                }
+                extraItem = item;
+                item.OnPickUp();
+            }
+            else { }
+
+            if (ItemAdded != null)
+                ItemAdded(this, new TFIWItemEventArgs(item));
+
+        }
+    }
+
+
+}
