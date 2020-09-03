90069ce6c1d7d752d1efcef2ba92375c054f0c4a
Alonsowski <santanasamuel29@gmail.com>
2020-09-03 15:44:10 -0500

    Trabajo en escenario
    Plataformas giratorias y plataformas que se caen

---
+++ Prototype01/Assets/Scripts/map scripts/cylinderRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class cylinderRotation : MonoBehaviour
+{
+    public float velocity = 1f;
+
+
+    float velocityAd = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationVelocity = velocity * velocityAd;
+        transform.Rotate(new Vector3(0f, rotationVelocity, 0f));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            other.transform.parent.parent.parent = transform;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            other.transform.parent.parent.parent = null;
+    }
+}
---
+++ Prototype01/Assets/Scripts/map scripts/fallingTiles.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingTiles : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float time2fall = 1f;
+
+    float tiempo = 0f;
+    bool playerEntered = false;
+    bool tileFalling = false;
+    bool restartingPositon = false;
+    Vector3 initialPosition;
+    Quaternion initialRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        initialPosition = rb.transform.position;
+        initialRotation = rb.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.isKinematic && !restartingPositon)
+        {
+            if (playerEntered)
+                tiempo = tiempo + 1 * Time.deltaTime;
+            if (tiempo >= time2fall)
+            {
+                rb.isKinematic = false;
+                tileFalling = true;
+            }
+        }
+        else
+        {
+            if(tileFalling)
+            {
+                if (rb.transform.position.y <= (initialPosition.y - 10f))
+                {
+                    restartingPositon = true;
+                    rb.useGravity = false;
+                    tileFalling = false;
+                }
+            }
+            else
+            {
+                if (rb.transform.position.y <= initialPosition.y)
+                    rb.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y + 0.2f, rb.transform.position.z);
+                else
+                {
+                    rb.transform.rotation = initialRotation;
+                    rb.transform.position = initialPosition;
+                    restartingPositon = false;
+                    playerEntered = false;
+                    rb.isKinematic = true;
+                    rb.useGravity = true;
+                    tiempo = 0f;
+                }    
+            }
+        }
+
+            
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            playerEntered = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
